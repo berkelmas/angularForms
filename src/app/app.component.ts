bc6897bf99eefcc082842d4d5b7d6f6ca23341b1
unknown <berkelmas96@gmail.com>
2019-06-14 13:37:54 +0300

    Angular Reactive Forms - first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl
+ } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reactiveFormsOdev';
+  defaultForm = new FormGroup({
+    projectName: new FormControl(null,
+
+    // Here is normal validators
+    [
+      Validators.required,
+      // Custom validator which does not allow test as proejctName field's value.
+      (control: AbstractControl) => {
+        if (control.value === "Test") {
+          return {"testAsName" : true};
+        } else {
+          return null;
+        }
+      }
+    ],
+
+    // Here is async validators
+    [
+      (control: AbstractControl) => new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'berk') {
+            resolve({"berkAsName" : true});
+          } else {
+            resolve(null);
+          }
+        }, 2000)
+      })
+    ]
+    ),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.email
+    ]),
+    projectStatus: new FormControl('Critical')
+  })
+
+  onSubmit() {
+    console.log(this.defaultForm.value)
+  }
+
 }
